1bf14eae40c3ca9c8226e83374a4d7b460333e76
Amir Hamza <hamza_amir@yahoo.com>
2019-09-09 06:22:31 +0600

    model and validation

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const connectDB = 	require('./config/db');
+const connectDB = require('./config/db');
 
 const app = express();
 
 connectDB();
 
-app.get('/', (req,res) => res.send(`API Running`));
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send(`API Running`));
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
